6905a09573936a151fe1c294c149964d6bb35076
Snow <511422713@qq.com>
2019-10-22 22:27:07 -0700

    All Dynamic Pages

--- demo/pages/detail/detail.js
+++ demo/pages/detail/detail.js
@@ -1,13 +1,16 @@
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    currentTab: 0
+    currentTab: 0,
+    apartmentDetail: ''
   },
 
-  swichNav: function (e) {
+  swichNav: function(e) {
     if (this.data.currentTab === e.target.dataset.current) {
       return false;
     } else {
@@ -17,63 +20,80 @@ Page({
     }
   },
 
-  stopTouchMove: function () {
+  stopTouchMove: function() {
     return false;
+  },
+
+  getApartmentDetail: function(id) {
+    var myThis = this
+    wx.request({
+      url: app.globalData.URL + 'getRoomDetailsById?_id=' + id,
+      method: 'GET',
+      data: {},
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function(res) {
+        myThis.setData({
+          apartmentDetail: res.data[0]
+        });
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.getApartmentDetail(options._id)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- demo/pages/index/index.js
+++ demo/pages/index/index.js
@@ -1,65 +1,118 @@
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    region: 'LA',
+    activities: [],
+    apartments: []
+  },
 
+  makeAppointment: function() {
+    wx.navigateTo({
+      url: '../contact/contact',
+    })
+  },
+
+  selectRegion: function() {},
+
+  viewApartmentDetail: function(e) {
+    wx.navigateTo({
+      url: '../detail/detail?_id=' + e.currentTarget.dataset._id
+    })
+  },
+
+  getActivities: function() {
+    var myThis = this
+    wx.request({
+      url: app.globalData.URL + 'getLatestActivities',
+      method: 'GET',
+      data: {},
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function(res) {
+        myThis.setData({
+          activities: res.data
+        });
+      }
+    })
+  },
+
+  getApartments: function() {
+    var myThis = this
+    wx.request({
+      url: app.globalData.URL + 'getRoomListByRegion?region=' + this.data.region,
+      method: 'GET',
+      data: {},
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function(res) {
+        myThis.setData({
+          apartments: res.data
+        });
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.getActivities()
+    this.getApartments()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
