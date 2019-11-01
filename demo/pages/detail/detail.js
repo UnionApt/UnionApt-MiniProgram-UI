f42abb35556738394b55776666c4b9ed3309aac1
Snow <511422713@qq.com>
2019-11-01 16:00:39 -0700

    Update Detail Page

--- demo/pages/detail/detail.js
+++ demo/pages/detail/detail.js
@@ -7,7 +7,14 @@ Page({
    */
   data: {
     currentTab: 0,
-    apartmentDetail: ''
+    apartmentDetail: '',
+    downloading: false
+  },
+
+  contact: function() {
+    wx.navigateTo({
+      url: '../contact/contact',
+    })
   },
 
   download: function() {
@@ -25,60 +32,64 @@ Page({
       }
     })
 
-    wx.showToast({
-      title: '下载海报中',  //标题
-      icon: 'loading',  //图标，支持"success"、"loading"
-      image: '',  //自定义图标的本地路径，image 的优先级高于 icon
-      duration: 5000, //提示的延迟时间，单位毫秒，默认：1500
-      mask: false,  //是否显示透明蒙层，防止触摸穿透，默认：false
-      success: function () { }, //接口调用成功的回调函数
-      fail: function () { },  //接口调用失败的回调函数
-      complete: function () { } //接口调用结束的回调函数
-    })
+    //防止多次点击
+    if (!this.data.downloading) {
+      this.data.downloading = true
 
-    //文件下载
-    var imgSrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXJjQM10tLcTErnKQb9XaYnm813S8_rwlI0vqMX1qVIoGKoIXg7g&s"
-    wx.downloadFile({
-      url: imgSrc,
-      success: function(res) {
-        console.log(res);
-        //图片保存到本地
-        wx.saveImageToPhotosAlbum({
-          filePath: res.tempFilePath,
-          success: function(data) {
-            console.log(data);
-            wx.hideToast();
-            wx.showToast({
-              title: '已下载海报到相册',  //标题
-              icon: 'success',  //图标，支持"success"、"loading"
-              image: '',  //自定义图标的本地路径，image 的优先级高于 icon
-              duration: 1500, //提示的延迟时间，单位毫秒，默认：1500
-              mask: false,  //是否显示透明蒙层，防止触摸穿透，默认：false
-              success: function () { }, //接口调用成功的回调函数
-              fail: function () { },  //接口调用失败的回调函数
-              complete: function () { } //接口调用结束的回调函数
-            })
-          },
-          fail: function(err) {
-            console.log(err);
-            if (err.errMsg === "saveImageToPhotosAlbum:fail auth deny") {
-              console.log("用户一开始拒绝了，我们想再次发起授权")
-              console.log('打开设置窗口')
-              wx.openSetting({
-                success(settingdata) {
-                  console.log(settingdata)
-                  if (settingdata.authSetting['scope.writePhotosAlbum']) {
-                    console.log('获取权限成功，给出再次点击图片保存到相册的提示。')
-                  } else {
-                    console.log('获取权限失败，给出不给权限就无法正常使用的提示')
-                  }
-                }
+      wx.showToast({
+        title: '下载海报中', //标题
+        icon: 'loading', //图标，支持"success"、"loading"
+        image: '', //自定义图标的本地路径，image 的优先级高于 icon
+        duration: 5000, //提示的延迟时间，单位毫秒，默认：1500
+        mask: false, //是否显示透明蒙层，防止触摸穿透，默认：false
+        success: function() {}, //接口调用成功的回调函数
+        fail: function() {}, //接口调用失败的回调函数
+        complete: function() {} //接口调用结束的回调函数
+      })
+
+      //文件下载
+      var imgSrc = "http://13.57.203.205:8000/public/images/poster.png"
+      var myThis = this
+      wx.downloadFile({
+        url: imgSrc,
+        success: function(res) {
+          myThis.data.downloading = false;
+          wx.hideToast();
+          //图片保存到本地
+          wx.saveImageToPhotosAlbum({
+            filePath: res.tempFilePath,
+            success: function(data) {
+              wx.showToast({
+                title: '已下载海报到相册', //标题
+                icon: 'success', //图标，支持"success"、"loading"
+                image: '', //自定义图标的本地路径，image 的优先级高于 icon
+                duration: 1500, //提示的延迟时间，单位毫秒，默认：1500
+                mask: false, //是否显示透明蒙层，防止触摸穿透，默认：false
+                success: function() {}, //接口调用成功的回调函数
+                fail: function() {}, //接口调用失败的回调函数
+                complete: function() {} //接口调用结束的回调函数
               })
+            },
+            fail: function(err) {
+              if (err.errMsg === "saveImageToPhotosAlbum:fail auth deny") {
+                console.log("用户一开始拒绝了，我们想再次发起授权")
+                console.log('打开设置窗口')
+                wx.openSetting({
+                  success(settingdata) {
+                    console.log(settingdata)
+                    if (settingdata.authSetting['scope.writePhotosAlbum']) {
+                      console.log('获取权限成功，给出再次点击图片保存到相册的提示。')
+                    } else {
+                      console.log('获取权限失败，给出不给权限就无法正常使用的提示')
+                    }
+                  }
+                })
+              }
             }
-          }
-        })
-      }
-    })
+          })
+        }
+      })
+    }
   },
 
   swichNav: function(e) {
