42a1a6392ba3d830f2ea3c494f68ea901e4bd760
Snow <511422713@qq.com>
2019-12-26 14:50:05 -0800

    Add key

--- demo/app.js
+++ demo/app.js
@@ -5,10 +5,22 @@ App({
    * 全局变量
    */
   globalData: {
-    URL: 'http://39.106.187.76:8000/'
+    URL: 'http://39.106.187.76:8000/',
+    KEY: ''
   },
 
   onLaunch: function() {
-    
+    var myThis = this
+    wx.request({
+      url: this.globalData.URL + 'getUnionKey',
+      method: 'GET',
+      data: {},
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function (res) {
+        myThis.globalData.KEY = res.data
+      }
+    })
   },
 })
