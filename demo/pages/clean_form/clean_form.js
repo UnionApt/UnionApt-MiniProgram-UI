d0669cdc8b26a9beb76bf0c51555c507913556ca
Snow <511422713@qq.com>
2019-12-27 00:47:24 -0800

    Update clean

--- demo/pages/clean_form/clean_form.js
+++ demo/pages/clean_form/clean_form.js
@@ -1,7 +1,7 @@
 // pages/clean_form/clean_form.js
+const app = getApp()
 var date = new Date();
 var currentHours = date.getHours();
-var currentMinute = date.getMinutes();
 
 Page({
 
@@ -21,9 +21,8 @@ Page({
     multiArray: [
       ['今天', '明天', '3-2', '3-3', '3-4', '3-5'],
       [0, 1, 2, 3, 4, 5, 6],
-      [0, 10, 20]
     ],
-    multiIndex: [0, 0, 0],
+    multiIndex: [0, 0],
   },
 
   setCommunity(e) {
@@ -39,19 +38,54 @@ Page({
       _email,
       _key,
       _remark,
-    } = e.detail.value
-
-    wx.showModal({
-      title: '预约成功',
-      content: '确认邮件已发送至您的邮箱',
-      showCancel: false,
-      confirmText: '返回首页',
-      success(res) {
-        if (res.confirm) {
-          console.log('返回首页')
-        }
+    } = e.detail.value;
+    if (!_name || !_roomId || !_email || !_key || !_remark || !this.data.community || this.data.startDate == '请选择日期') {
+      wx.showToast({
+        title: '请填写全部标有*的信息',
+        icon: 'none',
+        duration: 2000
+      })
+    } else {
+      if (_key != app.globalData.KEY) {
+        wx.showToast({
+          title: '请输入正确的产品密钥',
+          icon: 'none',
+          duration: 2000
+        })
+        return
       }
-    })
+      wx.request({
+        url: app.globalData.URL + 'service/cleaningorder/submit',
+        method: 'POST',
+        header: {
+          'content-type': 'application/json'
+        },
+        data: {
+          name: _name,
+          email: _email,
+          room: _roomId,
+          requirement: _remark,
+          community: this.data.community,
+          time: this.data.startDate,
+        },
+        success: function(res) {
+          console.log(res) // 服务器回包信息
+          wx.showModal({
+            title: '预定成功',
+            content: '确认邮件已发送至您的邮箱',
+            showCancel: false,
+            confirmText: '返回首页',
+            success(res) {
+              if (res.confirm) {
+                wx.navigateBack({
+                  delta: 3
+                })
+              }
+            }
+          })
+        }
+      })
+    }
   },
 
   /**
@@ -115,13 +149,18 @@ Page({
 
     var monthDay = ['今天', '明天'];
     var hours = [];
-    var minute = [];
+    var interval = 2;
 
     currentHours = date.getHours();
-    currentMinute = date.getMinutes();
+
+    if (currentHours >= 18) {
+      monthDay = ['明天', '后天'];
+      interval = 3;
+      currentHours = 1;
+    }
 
     // 月-日
-    for (var i = 2; i <= 28; i++) {
+    for (var i = interval; i <= 10; i++) {
       var date1 = new Date(date);
       date1.setDate(date.getDate() + i);
       var md = (date1.getMonth() + 1) + "-" + date1.getDate();
@@ -133,25 +172,13 @@ Page({
       multiIndex: this.data.multiIndex
     };
 
-    if (data.multiIndex[0] === 0) {
-      if (data.multiIndex[1] === 0) {
-        this.loadData(hours, minute);
-      } else {
-        this.loadMinute(hours, minute);
-      }
-    } else {
-      this.loadHoursMinute(hours, minute);
-    }
+    this.loadHours(hours);
 
     data.multiArray[0] = monthDay;
     data.multiArray[1] = hours;
-    data.multiArray[2] = minute;
 
     this.setData(data);
   },
-
-
-
 
   bindMultiPickerColumnChange: function(e) {
     date = new Date();
@@ -160,10 +187,15 @@ Page({
 
     var monthDay = ['今天', '明天'];
     var hours = [];
-    var minute = [];
+    var interval = 2;
 
     currentHours = date.getHours();
-    currentMinute = date.getMinutes();
+
+    if (currentHours >= 18) {
+      monthDay = ['明天', '后天'];
+      interval = 3;
+      currentHours = 1;
+    }
 
     var data = {
       multiArray: this.data.multiArray,
@@ -172,135 +204,21 @@ Page({
     // 把选择的对应值赋值给 multiIndex
     data.multiIndex[e.detail.column] = e.detail.value;
 
-    // 然后再判断当前改变的是哪一列,如果是第1列改变
-    if (e.detail.column === 0) {
-      // 如果第一列滚动到第一行
-      if (e.detail.value === 0) {
-
-        that.loadData(hours, minute);
-
-      } else {
-        that.loadHoursMinute(hours, minute);
-      }
-
+    // 然后再判断当前改变的是哪一列
+    if (e.detail.column === 0) { //如果是第1列改变
+      that.loadHours(hours);
       data.multiIndex[1] = 0;
-      data.multiIndex[2] = 0;
-
-      // 如果是第2列改变
-    } else if (e.detail.column === 1) {
-
-      // 如果第一列为今天
-      if (data.multiIndex[0] === 0) {
-        if (e.detail.value === 0) {
-          that.loadData(hours, minute);
-        } else {
-          that.loadMinute(hours, minute);
-        }
-        // 第一列不为今天
-      } else {
-        that.loadHoursMinute(hours, minute);
-      }
-      data.multiIndex[2] = 0;
-
-      // 如果是第3列改变
-    } else {
-      // 如果第一列为'今天'
-      if (data.multiIndex[0] === 0) {
-
-        // 如果第一列为 '今天'并且第二列为当前时间
-        if (data.multiIndex[1] === 0) {
-          that.loadData(hours, minute);
-        } else {
-          that.loadMinute(hours, minute);
-        }
-      } else {
-        that.loadHoursMinute(hours, minute);
-      }
+    } else if (e.detail.column === 1) { //如果是第2列改变
+      that.loadHours(hours);
     }
+
     data.multiArray[1] = hours;
-    data.multiArray[2] = minute;
     this.setData(data);
   },
 
-  loadData: function(hours, minute) {
-
-    var minuteIndex;
-    if (currentMinute > 0 && currentMinute <= 10) {
-      minuteIndex = 10;
-    } else if (currentMinute > 10 && currentMinute <= 20) {
-      minuteIndex = 20;
-    } else if (currentMinute > 20 && currentMinute <= 30) {
-      minuteIndex = 30;
-    } else if (currentMinute > 30 && currentMinute <= 40) {
-      minuteIndex = 40;
-    } else if (currentMinute > 40 && currentMinute <= 50) {
-      minuteIndex = 50;
-    } else {
-      minuteIndex = 60;
-    }
-
-    if (minuteIndex == 60) {
-      // 时
-      for (var i = currentHours + 1; i >= 10 && i <= 18; i++) {
-        hours.push(i);
-      }
-      // 分
-      for (var i = 0; i < 60; i += 10) {
-        minute.push(i);
-      }
-    } else {
-      // 时
-      for (var i = currentHours; i >= 10 && i <= 18; i++) {
-        hours.push(i);
-      }
-      // 分
-      for (var i = minuteIndex; i < 60; i += 10) {
-        minute.push(i);
-      }
-    }
-  },
-
-  loadHoursMinute: function(hours, minute) {
-    // 时
-    for (var i = 10; i <= 18; i++) {
+  loadHours: function(hours) {
+    for (var i = Math.max(10, currentHours + 1); i < 18; i++) {
       hours.push(i);
-    }
-    // 分
-    for (var i = 0; i < 60; i += 10) {
-      minute.push(i);
-    }
-  },
-
-  loadMinute: function(hours, minute) {
-    var minuteIndex;
-    if (currentMinute > 0 && currentMinute <= 10) {
-      minuteIndex = 10;
-    } else if (currentMinute > 10 && currentMinute <= 20) {
-      minuteIndex = 20;
-    } else if (currentMinute > 20 && currentMinute <= 30) {
-      minuteIndex = 30;
-    } else if (currentMinute > 30 && currentMinute <= 40) {
-      minuteIndex = 40;
-    } else if (currentMinute > 40 && currentMinute <= 50) {
-      minuteIndex = 50;
-    } else {
-      minuteIndex = 60;
-    }
-
-    if (minuteIndex == 60) {
-      // 时
-      for (var i = currentHours + 1; i >= 10 && i <= 18; i++) {
-        hours.push(i);
-      }
-    } else {
-      // 时
-      for (var i = currentHours; i >= 10 && i <= 18; i++) {
-        hours.push(i);
-      }
-    }
-    // 分
-    for (var i = 0; i < 60; i += 10) {
-      minute.push(i);
     }
   },
 
@@ -308,7 +226,6 @@ Page({
     var that = this;
     var monthDay = that.data.multiArray[0][e.detail.value[0]];
     var hours = that.data.multiArray[1][e.detail.value[1]];
-    var minute = that.data.multiArray[2][e.detail.value[2]];
 
     if (monthDay === "今天") {
       var month = date.getMonth() + 1;
@@ -318,14 +235,16 @@ Page({
       var date1 = new Date(date);
       date1.setDate(date.getDate() + 1);
       monthDay = (date1.getMonth() + 1) + "月" + date1.getDate() + "日";
-
+    } else if (monthDay === "后天") {
+      var date1 = new Date(date);
+      date1.setDate(date.getDate() + 2);
+      monthDay = (date1.getMonth() + 1) + "月" + date1.getDate() + "日";
     } else {
-      var month = monthDay.split("-")[0]; // 返回月
-      var day = monthDay.split("-")[1]; // 返回日
+      var month = monthDay.split("-")[0];
+      var day = monthDay.split("-")[1];
       monthDay = month + "月" + day + "日";
     }
-
-    var startDate = monthDay + " " + hours + ":" + minute;
+    var startDate = monthDay + " " + hours + ":00"
     that.setData({
       startDate: startDate
     })
